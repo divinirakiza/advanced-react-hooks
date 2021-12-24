ba6fc1f699eba4183b125e2d5c8ba805337a81c0
DivinIrakiza <irakizadivin@gmail.com>
2021-12-24 15:32:06 +0200

    chore: useState object states

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,28 @@ const App = () => {
   // Renders only on initial state.
   useState(() => expensiveInitialState());
 
-  const [count, setCount] = useState(0);
+  // Base Setter
+  // const [count, setCount] = useState(0);
+
+  const [{count1, count2}, setCount] = useState({count1: 0, count2: 1});
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Count</button>
-      <p>{count}</p>
+      {/* Will override the whole object */}
+      {/* <button onClick={() => setCount(curr => {count1: (curr.count1 + 1)} )}>+</button> */}
+    
+      <button onClick={() => 
+                              setCount(curr => (
+                                { ...curr,
+                                  count1: (curr.count1 + 1) 
+                                  
+                                }))}>+</button>
+      
+      {/* // No overwriting updates */}
+      {/* <button onClick={() => setCount(count => (count + 1))}>Count 3</button> */}
+
+      <p>Count1: {count1}</p>
+      <p>Count2: {count2}</p>
     </div>
   )
 
