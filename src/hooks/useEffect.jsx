1d4a9e10e2520ea86df0a9cdc3a176b4c9a38643
DivinIrakiza <irakizadivin@gmail.com>
2021-12-31 08:33:32 +0200

    [chore]: added useEffect usage

--- src/hooks/useEffect.jsx
+++ src/hooks/useEffect.jsx
@@ -1,12 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useForm} from './custom';
+import Hello from '../components/Hello';
 
 export function UseEffectUsage() {
+    const [values, handleChange] = useForm({
+        email: '', 
+        firstName: '',
+        password: ''
+    });
+    const [showHello, setShowHello] = useState(true);
+ 
+
     useEffect(() => {
         console.log('render');
-    })
+
+        return () => {
+            console.log('unmount');
+        }
+    }, [values, showHello]);
 
     return (
-        <div>Use effect</div>
+        <div>
+            <div>
+             <h4>Use effect</h4>
+
+                <div>
+                    <button type="button" onClick={() => setShowHello(!showHello)}>Toogle</button>
+                    {showHello && <Hello/>}
+                                    </div>
+             <input type="text" value={values.email} onChange={handleChange} name='email' placeholder="email" />
+             <input type="password" value={values.password} onChange={handleChange} name='password' placeholder="Passowrd" />
+            </div>
+        </div>
     )
 }
 
